a2045982ff5363b9dfc9087f66889526a60bac71
ValentinesInvite <xdeereel@gmail.com>
2024-03-31 06:31:09 +0800

    Finished Cart and Order Confirm Functions

--- Javascript/login.js
+++ Javascript/login.js
@@ -1,22 +1,22 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   const loginbutton = document.getElementById("loginbutton");
-//   loginbutton.addEventListener("click", login);
-// });
+document.addEventListener("DOMContentLoaded", () => {
+  const loginbutton = document.getElementById("loginbutton");
+  loginbutton.addEventListener("click", login);
+});
 
-// function login() {
-//   const username = document.getElementById("username_input").value;
-//   const password = document.getElementById("password_input").value;
-//   const getUsername = sessionStorage.getItem("user_username");
-//   const getPassword = sessionStorage.getItem("user_password");
+function login() {
+  const username = document.getElementById("username_input").value;
+  const password = document.getElementById("password_input").value;
+  const getUsername = sessionStorage.getItem("user_username");
+  const getPassword = sessionStorage.getItem("user_password");
 
-//   if (username === "CSELEC03" && password === "webprog") {
-//     sessionStorage.setItem("logged", true);
-//     sessionStorage.setItem("user", username);
-//     window.location.href = "./index.html";
-//   } else if (getUsername === username && getPassword === password) {
-//     sessionStorage.setItem("logged", true);
-//     window.location.href = "./index.html";
-//   } else {
-//     alert("Invalid Username or password. Please try again");
-//   }
-// }
+  if (username === "CSELEC03" && password === "webprog") {
+    sessionStorage.setItem("logged", true);
+    sessionStorage.setItem("user", username);
+    window.location.href = "./index.html";
+  } else if (getUsername === username && getPassword === password) {
+    sessionStorage.setItem("logged", true);
+    window.location.href = "./index.html";
+  } else {
+    alert("Invalid Username or password. Please try again");
+  }
+}
