592a26b58804a645a083ad7dfa1359ebece860eb
ShaunIvan <shaunivanmonillas03@gmail.com>
2024-03-30 23:29:58 +0800

    Updated Product Page

--- Javascript/product.js
+++ Javascript/product.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const productDetails = JSON.parse(localStorage.getItem("productItem"));
-
+  
   if (productDetails) {
     document.querySelector(".item_image").src = productDetails.image;
     document.querySelector(".item_name").textContent = productDetails.name;
     document.querySelector(".item_price").textContent = productDetails.price;
     // Assuming you have a place to show the detailed description
-    document.querySelector(".item_description").textContent =
+    document.querySelector(".item_details").textContent = productDetails.details;
+    
       productDetails.details || "No additional details available.";
   } else {
     console.log("No product details found in localStorage.");
