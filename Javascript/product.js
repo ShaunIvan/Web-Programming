0e86542f4b6bbf5e1b84ed4aa47c97650ec1f3b9
ShaunIvan <shaunivanmonillas03@gmail.com>
2024-03-31 07:03:30 +0800

    Updated Product Page

--- Javascript/product.js
+++ Javascript/product.js
@@ -1,4 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // const productDetails = JSON.parse(localStorage.getItem("productItem"));
+  
+  // if (productDetails) {
+  //   document.querySelector(".item_image").src = productDetails.image;
+  //   document.querySelector(".item_name").textContent = productDetails.name;
+  //   document.querySelector(".item_price").textContent = productDetails.price;
+  //   // Assuming you have a place to show the detailed description
+  //   document.querySelector(".item_details").textContent = productDetails.details;
+   
+  //     productDetails.details || "No additional details available.";
+  // } else {
+  //   console.log("No product details found in localStorage.");
+  // }
+  const addCart = document.querySelector(".addToCart");
+  addCart.addEventListener('click', addToCart);
+  
+
+  productLoad();
+});
+
+function productLoad () {
   const productDetails = JSON.parse(localStorage.getItem("productItem"));
   
   if (productDetails) {
@@ -12,4 +33,34 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     console.log("No product details found in localStorage.");
   }
-});
+}
+
+function addToCart(event) {
+  var button = event.target;
+  var parentContainer = button.closest(".product__item");
+
+  var itemImage = parentContainer.querySelector(".item_image").src;
+  var itemName = parentContainer.querySelector(".item_name").textContent;
+  var itemPrice = parentContainer.querySelector(".item_price").textContent;
+  var dataPrice = parentContainer
+    .querySelector(".item_price")
+    .getAttribute("data-price");
+
+  let itemDetails = {
+    image: itemImage,
+    name: itemName,
+    price: itemPrice,
+    dataP: dataPrice,
+  };
+
+  let product_items = JSON.parse(localStorage.getItem("product_items")) || [];
+
+  product_items.push(itemDetails);
+
+  localStorage.setItem("product_items", JSON.stringify(product_items));
+
+  var storedItem = localStorage.getItem("product_items");
+  var storedItemParse = JSON.parse(storedItem);
+
+  console.log(storedItemParse);
+}
